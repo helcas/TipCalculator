7e4dc30e3d088275b731b449947cf572bda38f93
helcas <helcas-1@student.ltu.se>
2022-04-27 11:12:35 +0200

    TipCalculator from course book

---
+++ src/main/java/com/example/tipcalculator/TipCalculator.java
@@ -0,0 +1,26 @@
+package com.example.tipcalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+
+import java.io.IOException;
+
+public class TipCalculator extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+      Parent root = FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
+      Scene scene = new Scene(root);
+      stage.setTitle("TipCalculator");
+      stage.setScene(scene);
+      stage.show();
+
+            }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
